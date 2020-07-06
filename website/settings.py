8aa5406845cdb9b47962cfabc3960b66adf9246f
iordanAlexandru <59307943+iordanAlexandru@users.noreply.github.com>
2020-07-06 16:52:54 +0300

    final_v3

--- website/settings.py
+++ website/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -143,3 +143,4 @@ STATICFILES_DIRS = [
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+django_heroku.settings(locals())
